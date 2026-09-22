0196838642e753ad0edbd410e33993992a72825a
Daniel Pascal O. <boogeyclub@gmail.com>
2026-09-22 09:30:23 +0100

    commit init 1

---
+++ Back-End/service-connectmarket/src/main/java/cm/odigital/serviceconnectmarket/ServiceConnectmarketApplication.java
@@ -0,0 +1,13 @@
+package cm.odigital.serviceconnectmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceConnectmarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceConnectmarketApplication.class, args);
+    }
+
+}
---
+++ Back-End/service-connectmarket/src/test/java/cm/odigital/serviceconnectmarket/ServiceConnectmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package cm.odigital.serviceconnectmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceConnectmarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
